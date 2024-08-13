a99f6232a7f3b5f3b14111e53bd6cecd26c1004e
Done83 <nedomehan@gmail.com>
2024-08-13 09:58:34 +0200

    useReducer and reducers function created

--- src/context.jsx
+++ src/context.jsx
@@ -1,14 +1,20 @@
 import { createContext, useContext, useReducer, useEffect } from "react";
+import reducer from "./reducer";
 
 const AppContext = createContext();
 
+const initialState = {
+  loading: false,
+  cart: [],
+};
+
 export const AppProvider = ({ children }) => {
-  const greeting = "hello";
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
 };
 
 export const useGlobalContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
